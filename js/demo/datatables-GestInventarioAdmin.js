a518bcd54e9f93b1bac6deb052a0635bbd074298
AbrahamR8A <a20206364@pucp.edu.pe>
2026-05-12 22:35:03 -0500

    Initial commit

---
+++ js/demo/datatables-GestInventarioAdmin.js
@@ -0,0 +1,39 @@
+let dataTable;
+
+const dataTableOptions = {
+  pageLength: 6,
+  destroy:true,
+
+  //scrollX: "2000px",
+
+  lengthMenu:[3, 5, 7],
+
+  columnDefs: [
+      {className:"centered",targets:[0, 1, 2, 3, 4, 5] },
+      {orderable: false,targets:[5]},
+      //{width: "15%",targets:[2] }
+    ],
+
+  language:{
+    lengthMenu: "Mostrar _MENU_ registros por pagina",
+    zeroRecords: "Ningún usuario encontrado",
+    info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+    infoEmpty: "Ningún usuario encontrado",
+    search: "Buscar",
+    loadingRecords: "Cargando...",
+    paginate: {
+      first: "Primero",
+      last: "Último",
+      next: "Siguiente",
+      previous: "Anterior",
+    }
+  }
+};
+
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  dataTable = $('#dataTable').DataTable(dataTableOptions);
+});
+
+
+
